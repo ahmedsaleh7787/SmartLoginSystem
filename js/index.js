cc9264e8a259fdfec522c794119bfcdf6809ce08
Ahmed Saleh <ahmed.saleh7787@gmail.com>
2025-06-18 10:57:47 +0300

    add addEventListener insteadof onclick

--- js/index.js
+++ js/index.js
@@ -30,6 +30,12 @@ if (JSON.parse(localStorage.getItem(LocalStorageKeyLogin))) {
 }
 
 
+var register=document.querySelector("#btn-register");
+
+if (register) {
+register.addEventListener('click',addUser);
+}
+
 function addUser() {
 
     if (existEmailTest(userEmail)) {
@@ -152,7 +158,11 @@ function existEmailLogin(element) {
 }
 
 
+var enterTo = document.querySelector('#btn-enterToSite');
 
+if (enterTo) {
+enterTo.addEventListener('click',enterToSite);
+}
 
 let loginUser = [];
 
--- js/mainpage.js
+++ js/mainpage.js
@@ -6,6 +6,9 @@
 
 document.querySelector("#welcomeUser").innerText = `Welcome, ${JSON.parse(localStorage.getItem("loginKey"))[0].newName}`;
 
+var logout = document.querySelector("#btn-logOut");
+logout.addEventListener('click',logOut);
+
 function logOut() {
     localStorage.removeItem("loginKey");
     window.location.href = "index.html";
