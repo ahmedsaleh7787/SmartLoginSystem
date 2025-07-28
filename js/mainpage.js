9825df2c7109c30c447821889d7d1e5d57c9d5d4
ahmedsaleh7787 <ahmed.saleh7787@gmail.com>
2025-07-28 18:25:10 +0300

    with cards

--- js/mainpage.js
+++ js/mainpage.js
@@ -15,3 +15,86 @@ function logOut() {
     window.location.href = "index.html";
 
 }
+
+
+
+
+
+
+// card ==============
+
+
+
+
+
+// async function getBusiness() {
+//     document.getElementById("loader").classList.replace('d-none', 'd-flex')
+//     document.getElementById("get").disabled = true;
+
+//     try {
+//         const response = await fetch(
+//             'https://script.google.com/macros/s/AKfycby4B6a8BCHZZ0KyLef-RmWgT8Msu4zR7t_2ABzi2e7qhDO9GN4EzDpd8grmax1hupUiLg/exec'
+//         );
+//         const data = await response.json();
+//         console.log(data);
+
+//         if (data.number) {
+//             document.getElementById("result").innerHTML = data.number;
+
+//             document.getElementById("qrcode").setAttribute("src", data.qr_code_url);
+
+//         } else {
+//             document.getElementById("result").innerHTML = data.message;
+
+//         }
+
+//     } catch (error) {
+//         console.log(error);
+//     } finally {
+//         document.getElementById("get").disabled = false;
+//         document.getElementById("loader").classList.replace('d-flex', 'd-none')
+//     }
+// }
+
+// document.getElementById("confirm").addEventListener("click", getBusiness);
+
+
+
+
+async function getBusiness() {
+    document.getElementById("loader").classList.replace('d-none', 'd-flex');
+    document.getElementById("get").disabled = true;
+
+    try {
+        const email = JSON.parse(localStorage.getItem("loginKey"))[0][0];
+
+        const formData = new FormData();
+        formData.append("email", email);
+
+        const response = await fetch(
+            'https://script.google.com/macros/s/AKfycbxAiXAmByr6Hf1FNDPD3jaVhSSBDjsqFWU2kWLx_COONKB2pYe3TEGsZavdH2iWE6MBmA/exec',
+            {
+                method: "POST",
+                body: formData
+            }
+        );
+
+        const data = await response.json();
+        console.log(data);
+
+        if (data.number) {
+            document.getElementById("result").innerHTML = data.number;
+            document.getElementById("qrcode").setAttribute("src", data.qr_code_url);
+        } else {
+            document.getElementById("result").innerHTML = data.message;
+        }
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+        document.getElementById("get").disabled = false;
+        document.getElementById("loader").classList.replace('d-flex', 'd-none');
+    }
+}
+
+document.getElementById("confirm").addEventListener("click", getBusiness);
